7ccfd6aead69a9bb20bd35cdac473c0ede776cd0
rufunt <jazzoo@mail.ru>
2018-01-20 15:08:41 +0300

    Add edit, apdate and destroy actions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+  before_action :find_message, only: [:show, :edit, :update, :destroy]
 
   def index
     @messages = Message.all.order("created_at DESC")
@@ -9,12 +10,10 @@ class MessagesController < ApplicationController
   end
 
   def show
-    @message = Message.find(params[:id])
   end
 
   def create
     @message = Message.new(message_params)
-
     if @message.save
       redirect_to root_path
     else
@@ -22,11 +21,29 @@ class MessagesController < ApplicationController
     end
   end
 
+  def edit
+  end
 
+  def update
+    if @message.update(message_params)
+      redirect_to message_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @message.destroy
+    redirect_to root_path
+  end
 
   private
 
   def message_params
     params.require(:message).permit(:title, :description)
+  end
+
+  def find_message
+    @message = Message.find(params[:id])
   end
 end
